27b67851a7235ce45dd9c786e2b110b2aa94dfdc
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-07-08 21:22:01 +0700

    add ticket component

--- src/Screen/Event/screen-event.js
+++ src/Screen/Event/screen-event.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Image } from 'react-native'
 import { View, Text, Container, Content } from 'native-base';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Ticket } from '../../Components/Ticket'
 function mapStateToProps(state) {
     return {
 
@@ -12,33 +14,43 @@ class ScreenEvent extends Component {
     render() {
         return (
             <Container>
-                <View style={{height:50}}>
+                <View style={{ height: 50 }}>
 
                 </View>
-                <Content style={{backgroundColor:'#FFF'}}>
-                    <View style={{ height: 120,backgroundColor:'#FFF' }}>
+                <Content style={{ backgroundColor: '#FFF' }}>
+                    <Ticket
+                        data={{ title: 'Reduce, Reuse, Recycle', date: 'Thursday, 12 May 2018', time: '10 PM' }}
+                        image={'https://marketplace.canva.com/MAB5W1orBoM/1/0/thumbnail_large/canva-environment-classroom-poster-MAB5W1orBoM.jpg'}
+                        bg_color={'#B2FF59'}
+                        sec_color={'#CCFF90'} />
+                    <Ticket
+                        data={{ title: 'Concert For Kid', date: 'Thursday, 12 May 2018', time: '10 PM' }}
+                        image={'https://marketplace.canva.com/MAB1GLyFELM/2/0/thumbnail_large/canva-concert-for-kids-fundraising-poster-MAB1GLyFELM.jpg'}
+                        bg_color={'#29B6F6'}
+                        sec_color={'#B3E5FC'} />
+                    <Ticket
+                        data={{ title: 'Blood Donation', date: 'Thursday, 12 May 2018', time: '10 PM' }}
+                        image={'https://marketplace.canva.com/MACXhzcB8NQ/4/0/thumbnail_large/canva-red-and-pink-blood-donation-poster-MACXhzcB8NQ.jpg'}
+                        bg_color={'#EF9A9A'}
+                        sec_color={'#FFCDD2'} />
+                    <Ticket
+                        data={{ title: 'Autumn Movie Marathon', date: 'Thursday, 12 May 2018', time: '10 PM' }}
+                        image={'https://marketplace.canva.com/MACGp2rUBsI/2/0/thumbnail_large/canva-burgundy-thanksgiving-movie-marathon-poster-MACGp2rUBsI.jpg'}
+                        bg_color={'#B71C1C'}
+                        sec_color={'#E65100'} />
+                    <Ticket
+                        data={{ title: 'Bright Music', date: 'Thursday, 12 May 2018', time: '10 PM' }}
+                        image={'https://marketplace.canva.com/MAB00NXwDDk/2/0/thumbnail_large/canva-music-poster-MAB00NXwDDk.jpg'}
+                        bg_color={'#80DEEA'}
+                        sec_color={'#4DD0E1'} />
 
-                        <View style={{ backgroundColor: '#FFECB3', height: 100, marginLeft:10,marginTop:10,marginRight:10, borderStyle:'dashed' }}>
-                            <View style={{height:'100%',width:'75%',borderRightWidth:1, borderStyle:'dashed',borderRadius:0.5,borderColor:'#FF9800'}}>
-
-                            </View>
-                        </View>
-                        <View style={{top:0,left:0,backgroundColor:'#FFF', width:25,height:25,borderRadius:20, position:'absolute'}}>
-
-                        </View>
-                        <View style={{left:0,bottom:0,backgroundColor:'#FFF', width:25,height:25,borderRadius:20, position:'absolute'}}>
-
-                        </View>
-                        <View style={{right:0,top:0,backgroundColor:'#FFF', width:25,height:25,borderRadius:20, position:'absolute'}}>
-
-                        </View>
-                        <View style={{right:0,bottom:0,backgroundColor:'#FFF', width:25,height:25,borderRadius:20, position:'absolute'}}>
-
-                        </View>
-                    </View>
-                                       
                     
+
+
+
+
                 </Content>
+
             </Container>
         );
     }
