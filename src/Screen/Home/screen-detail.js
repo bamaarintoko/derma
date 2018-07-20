0549ad9bec821d6ea7a18b5d2e29b0793257399a
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-07-21 03:19:44 +0700

    backup

--- src/Screen/Home/screen-detail.js
+++ src/Screen/Home/screen-detail.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Button, Container, Content, Text, Thumbnail, View} from "native-base";
-import {StatusBar} from "react-native";
+import {StatusBar, Dimensions} from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Api from "../../Utils/Api";
 import TimeAgo from 'react-native-timeago';
+import Carousel from 'react-native-snap-carousel';
+import Image from 'react-native-scalable-image';
+import moment from 'moment';
+const {width: viewportWidth, height: viewportHeight} = Dimensions.get('window');
+
+function wp(percentage) {
+    const value = (percentage * viewportWidth) / 100;
+    return Math.round(value);
+}
+
+const slideHeight = viewportHeight * 0.36;
+const slideWidth = wp(75);
+const itemHorizontalMargin = wp(2);
+
+export const sliderWidth = viewportWidth;
+export const itemWidth = slideWidth + itemHorizontalMargin * 2;
 
 class ScreenDetail extends Component {
     constructor(props) {
@@ -13,9 +29,13 @@ class ScreenDetail extends Component {
             user_photo: '',
             user_name: '',
             create_date: '',
-            title : '',
-            description:'',
-            img : []
+            title: '',
+            description: '',
+            img: [],
+            address: '',
+            location: '',
+            data: [],
+            endDonation:''
         }
     }
 
@@ -32,9 +52,13 @@ class ScreenDetail extends Component {
         Api._POST('reserve/detail_reserve', params)
             .then((response) => {
                 this.setState({
-                    title:response.data.result.reserve.reserve_title,
-                    description:response.data.result.reserve.reserve_description,
-                    img : response.data.result.image
+                    title: response.data.result.reserve.reserve_title,
+                    description: response.data.result.reserve.reserve_description,
+                    address: response.data.result.reserve.reserve_address,
+                    img: response.data.result.image,
+                    location: response.data.result.reserve.reserve_province + ", " + response.data.result.reserve.reserve_district + ", " + response.data.result.reserve.reserve_sub_district,
+                    data: JSON.parse(response.data.result.reserve.reserve_category),
+                    endDonation:moment(response.data.result.reserve.reserve_end_date).format('LL')
                 })
                 console.log(response)
             }).catch((err) => {
@@ -42,7 +66,20 @@ class ScreenDetail extends Component {
         })
     }
 
+    _renderItem({item, index}) {
+        return (
+            <View style={{backgroundColor: '#EEEEEE', alignItems: 'center', justifyContent: 'center'}}>
+                <Image
+                    style={{flex: 1}}
+                    height={300}
+
+                    source={{isStatic: true, uri: item.media_value}}/>
+            </View>
+        );
+    }
+
     render() {
+        console.log(this.state.data)
         return (
             <Container>
                 <StatusBar backgroundColor="#013976"/>
@@ -67,21 +104,67 @@ class ScreenDetail extends Component {
 
                     </View>
                 </View>
-                <Content style={{backgroundColor: '#FFF', padding: 5}}>
+                <Content style={{backgroundColor: '#FFF', padding: 5, paddingBottom: 10}}>
                     <View style={{flexDirection: 'row', alignItems: 'center'}}>
                         <View>
                             <Thumbnail source={{uri: this.state.user_photo}}/>
                         </View>
-                        <View style={{marginLeft:10}}>
+                        <View style={{marginLeft: 10}}>
                             <Text style={{fontSize: 12}}>{this.state.user_name}</Text>
                             <TimeAgo style={{fontSize: 12}} time={this.state.create_date}/>
                         </View>
                     </View>
-                    <View style={{marginTop:15}}>
-                        <Text style={{fontSize:14, fontWeight:'bold'}}>{this.state.title}</Text>
+                    <View style={{marginTop: 10}}>
+                        <Text style={{fontSize: 14, fontWeight: 'bold'}}>{this.state.title}</Text>
                     </View>
-                    <View style={{marginTop:15}}>
-                        <Text style={{fontSize:14}}>{this.state.description}</Text>
+                    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                        <Carousel
+                            ref={(c) => {
+                                this._carousel = c;
+                            }}
+                            data={this.state.img}
+                            renderItem={this._renderItem}
+                            sliderWidth={itemWidth}
+                            itemWidth={200}
+                        />
+                    </View>
+                    <View style={{marginTop: 10}}>
+                        <Text style={{fontSize: 14}}>{this.state.description}</Text>
+                    </View>
+                    <View style={{marginTop: 15, marginBottom: 10}}>
+                        <Text style={{fontSize: 14, fontWeight: 'bold'}}>What we need :</Text>
+                        <View style={{flexDirection: 'row'}}>
+                            <View style={{width:'20%'}}>
+                                <Text style={{fontSize:12, fontWeight:'bold'}}>Quantity</Text>
+                            </View>
+                            <View style={{width:'80%'}}>
+                                <Text style={{fontSize:12, fontWeight:'bold'}}>Category</Text>
+                            </View>
+                        </View>
+                        {
+                            this.state.data.map((v, k) => {
+                                return (
+                                    <View key={k} style={{flexDirection: 'row'}}>
+                                        <View style={{width:'20%'}}>
+                                            <Text style={{fontSize: 12}}>{v.count}</Text>
+                                        </View>
+                                        <View style={{width:'80%'}}>
+                                            <Text style={{fontSize: 12}}>{v.value}</Text>
+                                        </View>
+                                    </View>
+                                )
+                            })
+                        }
+
+                    </View>
+                    <View style={{marginTop: 10}}>
+                        <Text style={{fontSize: 14, fontWeight: 'bold'}}>Address :</Text>
+                        <Text style={{fontSize: 12,}}>{this.state.address}</Text>
+                        <Text style={{fontSize: 12,}}>{this.state.location}</Text>
+                    </View>
+                    <View style={{marginTop: 10, marginBottom: 10}}>
+                        <Text style={{fontSize: 14, fontWeight: 'bold'}}>End donation :</Text>
+                        <Text style={{fontSize: 12,}}>{this.state.endDonation}</Text>
                     </View>
                 </Content>
             </Container>
