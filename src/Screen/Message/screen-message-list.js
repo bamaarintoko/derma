13270da36be446f6ea211d17a7669eed27903fa1
bama <yodafatbama.arintko@gmail.com>
2018-08-27 22:12:26 +0700

    backup

--- src/Screen/Message/screen-message-list.js
+++ src/Screen/Message/screen-message-list.js
@@ -4,20 +4,22 @@ import {Button, Container, Content, List, ListItem, Thumbnail, Left, Body, Right
 import {StatusBar, View, FlatList} from "react-native"
 import Icon from "react-native-vector-icons/FontAwesome";
 import {sqlToJsISO} from "../../Utils/func";
+import Api from "../../Utils/Api";
+import TimeAgo from 'react-native-timeago';
 
 const email = [
     {
-        id:6,
+        id: 6,
         email: 'sinatriohappy.triaji@gmail.com',
         name: 'Happy'
     },
     {
-        id:2,
+        id: 2,
         email: 'yosafatbama.arintoko@gmail.com',
         name: 'Bama'
     },
     {
-        id:3,
+        id: 3,
         email: 'jonis8729@gmail.com',
         name: 'John Wick'
     }
@@ -30,20 +32,50 @@ function mapStateToProps(state) {
 }
 
 class ScreenMessageList extends Component {
-    componentDidMount() {
-        console.log("===>",this.props.redAuth.data.profile.user_email)
+    constructor(props) {
+        super(props);
+        this.state = {
+            data_message: [],
+            resp_message: "",
+            resp_kode: "",
+        }
     }
 
-    onPressConversation = (email,name,id)=>{
-        return ()=>{
+    componentDidMount() {
+        let params = {
+            par_user_id: this.props.redAuth.data.profile.user_id
+        }
+
+        Api._POST('message/get_messages', params)
+            .then((response) => {
+                console.log(response)
+                this.setState({
+                    data_message: response.data.data,
+                    resp_kode: response.data.kode,
+                    resp_message: response.data.message
+                })
+            }).catch((err) => {
+            this.setState({
+                data_message: [],
+                resp_kode: err.response.status.toString(),
+                resp_message: err.message
+            })
+        })
+        // console.log("===>",this.props.redAuth.data.profile.user_id)
+    }
+
+    onPressConversation = (email, name, id) => {
+        return () => {
             this.props.navigation.navigate('Conversation', {
                 email: email,
                 id: id,
-                name:name
+                name: name
             })
         }
     }
+
     render() {
+        console.log(this.state.resp_kode)
         return (
             <Container style={{backgroundColor: '#FFF'}}>
                 <StatusBar backgroundColor="#013976"/>
@@ -67,31 +99,58 @@ class ScreenMessageList extends Component {
 
                     </View>
                 </View>
-                <FlatList
-                    showsHorizontalScrollIndicator={false}
-                    data={email}
-                    keyExtractor={(item, index) => '' + index}
-                    renderItem={({item}) =>
-                        item.email !== this.props.redAuth.data.profile.user_email
-                        &&
-                        <List>
-                            <ListItem avatar onPress={this.onPressConversation(item.email, item.name, item.id)}>
-                                <Left>
-                                    <Thumbnail
-                                        source={{uri: 'http://debu.mlskoding.com/public/user/4115-1532421046.jpeg'}}/>
-                                </Left>
-                                <Body>
-                                <Text>{item.name}</Text>
-                                <Text note>Doing what you like will always keep you happy . .</Text>
-                                </Body>
-                                <Right>
-                                    <Text note>3:43 pm</Text>
-                                </Right>
-                            </ListItem>
-                        </List>
+                {
+                    this.state.resp_kode === "00"
+                        ?
+                        <FlatList
+                            showsHorizontalScrollIndicator={false}
+                            data={this.state.data_message}
+                            keyExtractor={(item, index) => '' + index}
+                            renderItem={({item}) =>
+                                item.user.user_email !== this.props.redAuth.data.profile.user_email
+                                    ?
+                                    <List>
+                                        <ListItem avatar
+                                                  onPress={this.onPressConversation(item.user.user_email, item.user.user_name, item.user.user_id)}>
+                                            <Left>
+                                                <Thumbnail
+                                                    source={{uri: item.user.user_photo}}/>
+                                            </Left>
+                                            <Body>
+                                            <Text>{item.user.user_name}</Text>
+                                            <Text note>{item.reply.reply}</Text>
+                                            </Body>
+                                            <Right>
+                                                <TimeAgo style={{fontSize: 12}}
+                                                         time={sqlToJsISO(item.reply.create_date)}/>
+                                            </Right>
+                                        </ListItem>
+                                    </List>
+                                    :
+                                    <List>
+                                        <ListItem avatar
+                                                  onPress={this.onPressConversation(item.from.user_email, item.from.user_name, item.from.user_id)}>
+                                            <Left>
+                                                <Thumbnail
+                                                    source={{uri: item.from.user_photo}}/>
+                                            </Left>
+                                            <Body>
+                                            <Text>{item.from.user_name}</Text>
+                                            <Text note>{item.reply.reply}</Text>
+                                            </Body>
+                                            <Right>
+                                                <TimeAgo style={{fontSize: 12}}
+                                                         time={sqlToJsISO(item.reply.create_date)}/>
+                                            </Right>
+                                        </ListItem>
+                                    </List>
 
-                    }
-                />
+                            }
+                        />
+                        :
+                        <Text>{this.state.resp_message}</Text>
+                }
+
 
             </Container>
         );
